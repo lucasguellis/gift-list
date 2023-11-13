027c12dfd71de3193c7c705063fbb23d023988fe
lucasguellis <luguellis@uolinc.com>
2023-11-13 19:56:19 -0300

    feat: Adding homepage to `/`

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                Olá, você não acessou uma rota existente :(
+            </header>
+        </div>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {Routes, Route} from "react-router";
+import {Routes, Route, Navigate} from "react-router";
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path=":name" element={<App/>}/>
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
