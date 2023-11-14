571f23b540054be23a7b0103b17b88a89c47320e
lucasguellis <luguellis@uolinc.com>
2023-11-14 09:27:36 -0300

    improv: adding loading state and prettier on `App.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { useEffect, useState } from "react";
 import { useParams } from 'react-router';
 import Checklist from './components/checklist';
@@ -12,13 +12,21 @@ function App(context) {
   const params = useParams();
 
   useEffect(() => {
-    const fetchData = async() => {
-      const result = await fetch(`${API_URL}/${params.name}`, {headers: {"Content-type": "application/json"}, method: "GET"})
-        .then(res => res.json())
+    setIsLoading(true);
+
+    const fetchData = async () => {
+      const result = await fetch(`${API_URL}/${params.name}`, {
+        headers: { "Content-type": "application/json" },
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .catch((err) => {
+          console.log(err);
+          return [];
+        });
       setData(result);
-    }
-    console.log("data: ");
-    console.log(data);
+      setIsLoading(false);
+    };
     fetchData();
   }, [setData, params.name, API_URL]);
 
@@ -26,49 +34,49 @@ function App(context) {
     const groupedData = data.reduce((acc, item) => {
       const tag = item.tag;
       if (!acc[tag]) {
-      acc[tag] = [];
+        acc[tag] = [];
       }
       acc[tag].push(item);
       return acc;
     }, {});
     setGroupedData(groupedData);
-    console.log("groupedData: ");
-    console.log(groupedData);
-  }, data)
+  }, [data]);
 
-  function handleCheckboxChange (tag, itemName, context) {
-      // Update the local state
-      const updatedData = data.map(item =>
-        item.tag === tag && item.name === itemName
-          ? { ...item, checked: !item.checked }
-          : item
-      );
-      console.log("updatedData");
-      console.log(updatedData);
+  function handleCheckboxChange(tag, itemName, context) {
+    // Update the local state
+    const updatedData = data.map((item) =>
+      item.tag === tag && item.name === itemName
+        ? { ...item, checked: !item.checked }
+        : item,
+    );
 
-      // Make the API request to update the checked property
-      fetch(`${API_URL}`, {
-      method: 'PUT',
+    // Make the API request to update the checked property
+    fetch(`${API_URL}`, {
+      method: "PUT",
       headers: {
-          'Content-Type': 'application/json',
-          "Access-Control-Allow-Origin": "*",
-          "Authentication": `X-Auth-Key ${context.env.AUTH_KEY_SECRET}`
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authentication: `X-Auth-Key ${context.env.AUTH_KEY_SECRET}`,
       },
-      body: JSON.stringify({"key": `gift-list-${params.name}.json`, "value": updatedData}),
-      }).then(res => res.json()).then(setData(updatedData)).catch(err => console.log(err));
+      body: JSON.stringify({
+        key: `gift-list-${params.name}.json`,
+        value: updatedData,
+      }),
+    })
+      .then((res) => res.json())
+      .then(setData(updatedData))
+      .catch((err) => console.log(err));
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        {data ? (
+        {isLoading ? <p>Carregando...</p> : (
           data.length > 0 ? (
             <Checklist data={groupedData} handler={handleCheckboxChange}/>
           ) : (
             <p>Essa lista não existe :(</p>
           )
-        ) : (
-          <p>Carregando...</p>
         )}
       </header>
     </div>
