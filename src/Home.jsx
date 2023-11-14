54adede298683c05a86df00d99ca47e0c02b2d84
lucasguellis <luguellis@uolinc.com>
2023-11-14 17:02:07 -0300

    teste functions

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function Home() {
+  const [teste, setTest] = useState('');
+  useEffect(() => {
+    const getEnv = async() => await fetch('envs/api_url', {
+      method: "GET",
+    }).then(res => setTest(res.text));
+    getEnv();
+  }, [setTest])
+  console.log(teste);
   return (
     <div className="App">
       <header className="App-header">
         Olá, você não acessou uma rota existente :(
       </header>
+      <p>{teste}</p>
     </div>
   );
 }
