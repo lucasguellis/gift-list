ecb718c17525e481ef9a2c7b0193b5ea26108749
lucasguellis <luguellis@uolinc.com>
2023-11-14 10:45:34 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [data, setData] = useState([]);
   const [groupedData, setGroupedData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const API_URL = process.env.API_URL;
+  const API_URL = (context) => context.env.API_URL;
   const AUTH_KEY_SECRET = process.env.AUTH_KEY_SECRET;
   const params = useParams();
 
